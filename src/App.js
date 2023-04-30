b027077bbc36ff99ff999af6f407457f52490229
abir-25 <abirbgc25@gmail.com>
2023-04-30 22:00:36 +0600

    Pricing Plan Section completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Price from "./components/Price/Price";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Price />
     </div>
   );
 }
---
+++ src/components/Price/Price.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Price.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+const Price = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header">
+        <span className="stroke-text">Ready to start</span>
+        <span>Your journey</span>
+        <span className="stroke-text">now with us</span>
+      </div>
+
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, j) => (
+                <div className="feature" key={j}>
+                  <img src={whiteTick} alt="" />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>See more benefits -></span>
+            </div>
+            <button className="btn">Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Price;
