c5e3b9ec794c40923bb7756deb08de60d87ac409
abir-25 <abirbgc25@gmail.com>
2023-05-01 05:35:04 +0600

    Hamburger menu implemented and responsive design completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../../assets/logo.png";
+import bars from "../../assets/bars.png";
+
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <div className="header">
       <img src={logo} alt="" className="logo" />
 
-      <ul className="headers-menu">
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      {!menuOpened && mobile ? (
+        <div
+          style={{
+            background: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setMenuOpened(true);
+          }}
+        >
+          <img
+            src={bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul
+          className="headers-menu"
+          onClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
+        </ul>
+      )}
     </div>
   );
 };
