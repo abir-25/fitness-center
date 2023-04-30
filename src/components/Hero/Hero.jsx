e9b0ee24362e53da8921f2b825327b0141d741c3
abir-25 <abirbgc25@gmail.com>
2023-05-01 04:07:17 +0600

    responsive design WIP

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   const transition = { type: "spring", duration: 1.5 };
+  const mobile = window.innerWidth <= 768 ? true : false;
   return (
     <div className="hero">
       <div className="blur hero-blur"></div>
@@ -18,7 +19,7 @@ const Hero = () => {
 
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "238px" }}
+            initial={{ left: mobile ? "165px" : "238px" }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
